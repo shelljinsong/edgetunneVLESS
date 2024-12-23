aba33473b830c8384c93cf613ef3ea0d9fb75c48
shelljinsong <70987881+shelljinsong@users.noreply.github.com>
2024-12-23 09:30:17 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,8 +2,8 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '594cb319-0fdb-4a18-b450-cd17f2a7489f';
-let proxyIP = 'sp.ceshi123321.tk';
-let sub = 'sp.ceshi123321.tk';
+let proxyIP = 'sp.ceshi123321.tk,www.baipiao.eu.org,proxyip.us.fxxk.dedyn.io ,proxyip.aliyun.fxxk.dedyn.io ';
+let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
 let subProtocol = 'https';
